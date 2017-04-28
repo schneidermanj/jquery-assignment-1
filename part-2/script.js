8ba557fb310eb6ae454011fdf6fc326e6e1eea72
schneidermanj <jennis@id.iit.edu>
2017-04-28 00:08:30 -0500

    part 2 assignment

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,24 @@
 // Do your work in this file.
+
+
+function isEven(n) {
+   return n % 2 == 0;
+}
+
+function isOdd(n) {
+   return Math.abs(n % 2) == 1;
+}
+
+for (var index = 1; index <= 196; index++) {
+    var box = document.createElement('div');
+    if (isEven(index)) {
+        box.className = "box type-1";
+    } else if (isOdd(index)) {
+        box.className = "box type-2";
+    }
+    $('body').append(box);
+}
+
+$('.box').click(function() {
+    $(this).toggleClass('clicked');
+});
